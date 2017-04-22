80d95a11a1454eaa9d0e25d0c8424e1e5d89f0e9
Louis McCarthy <compeoree@hotmail.com>
2017-04-22 10:27:57 -0500

    Initial FITS output working....needs more robust glitch handling and image appears to be split into even/odd frames

--- examples/parseRawImage.cpp
+++ examples/parseRawImage.cpp
@@ -7,6 +7,13 @@
   parseRawImage.ccp - Loads the raw image binary file and spits out debug info
 */
 
+#include <CCfits>
+#include <cmath>
+    // The library is enclosed in a namespace.
+
+    using namespace CCfits;
+    using std::valarray;
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -32,13 +39,51 @@ int main()
     int thisZeroIndex = 0;
     int lastZeroIndex = 0;
     int byteCount = 0;
-    for (long i=0; i<result; i++)
+    unsigned char lastByte;
+
+
+    // FITS Variables
+    long naxis = 2;
+    long naxes[naxis] = { 3110, 2034 };
+
+    std::auto_ptr<FITS> pFits(0);
+
+    try
     {
+        const std::string fileName("!sspro.fit");
+        pFits.reset( new FITS(fileName , USHORT_IMG , naxis , naxes ) );
+    }
+    catch (FITS::CantCreate)
+    {
+          return -1;
+    }
+
+    long& width = naxes[0];
+    //long& numberOfRows = naxes[1];
+    long nelements = std::accumulate(&naxes[0],&naxes[naxis],1,std::multiplies<long>());
+    std::valarray<unsigned short> row(width);
+    std::valarray<unsigned short> image(nelements);
+
+    for (unsigned long i=0; i<result; i++)
+    {
+        // create 16 bit values from 2 bytes
+        if (byteCount % 2 != 0)
+        {
+            //printf("Adding bytes 0x%X and 0x%X to get short 0x%X for array index %d\n", data[i], lastByte, ((unsigned short)data[i] << 8) + lastByte, (byteCount-1)/2);
+            row[(byteCount-1)/2] = ((unsigned short)data[i] << 8) + lastByte;
+        }
+
+        lastByte = data[i];
+
         if (++byteCount == 6220)
         {
             byteCount = 0;
             zeroCount = 0;
             printf("...good row\n");
+
+            // Add row to image
+            image[std::slice(width*static_cast<unsigned short>(rowCount),width,1)] = row + (unsigned short)rowCount;
+
             continue;
         }
 
@@ -53,16 +98,28 @@ int main()
                     lastZeroIndex = thisZeroIndex;
                 }
                 else
+                {
+                    //byteCount -= 18;
                     printf("...glitch...");
+                }
                 zeroCount = 0; // reset counter so a start byte of value 0x00 doesn't trip this again...
             }
         }
         else
         {
             if (zeroCount > 2 && zeroCount != 18)
-                printf("Found %d zeros in a row at location 0x%X\n", zeroCount, i);
+                printf("Found %d zeros in a row at location 0x%lX\n", zeroCount, (unsigned long)i);
             zeroCount = 0;
         }
     }
+
+    // Save image
+    long firstPixel(1);
+    long exposure(1500);
+    pFits->pHDU().addKey("EXPOSURE", exposure,"Total Exposure Time");
+
+    pFits->pHDU().write(firstPixel,nelements,image);
+    std::cout << pFits->pHDU() << std::endl;
+
     free(data);
 }
