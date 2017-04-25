b3f2e6e572407287e37deb362691080893ea37dd
Louis McCarthy <compeoree@gmail.com>
2017-04-24 22:35:20 -0500

    raw.image is split into odd rows followed by even rows. The parser now handles this.

--- examples/parseRawImage.cpp
+++ examples/parseRawImage.cpp
@@ -34,10 +34,7 @@ int main()
 
     printf("Read %lu bytes\n", result);
 
-    int zeroCount = 0;
     int rowCount = 0;
-    int thisZeroIndex = 0;
-    int lastZeroIndex = 0;
     int byteCount = 0;
     unsigned char lastByte;
 
@@ -59,11 +56,11 @@ int main()
     }
 
     long& width = naxes[0];
-    //long& numberOfRows = naxes[1];
     long nelements = std::accumulate(&naxes[0],&naxes[naxis],1,std::multiplies<long>());
     std::valarray<unsigned short> row(width);
     std::valarray<unsigned short> image(nelements);
 
+    printf("Parsing rows...");
     for (unsigned long i=0; i<result; i++)
     {
         // create 16 bit values from 2 bytes
@@ -78,41 +75,24 @@ int main()
         if (++byteCount == 6220)
         {
             byteCount = 0;
-            zeroCount = 0;
-            printf("...good row\n");
 
-            // Add row to image
-            image[std::slice(width*static_cast<unsigned short>(rowCount),width,1)] = row + (unsigned short)rowCount;
+            /* Add row to image (frames a+b, even's first)
+            if (rowCount > 1016)
+                image[std::slice(width*static_cast<unsigned short>((rowCount-1016)*2-1),width,1)] = row; // + (unsigned short)rowCount;
+            else
+                image[std::slice(width*static_cast<unsigned short>(rowCount*2),width,1)] = row;
+            rowCount++;*/
 
-            continue;
-        }
-
-        if (data[i] == 0x00)
-        {
-            if (++zeroCount == 18)
-            {
-                thisZeroIndex = i - 17;
-                if (byteCount == zeroCount)
-                {
-                    printf("Found row %d at index 0x%X, which is %d bytes since previous row", rowCount++, thisZeroIndex, thisZeroIndex - lastZeroIndex);
-                    lastZeroIndex = thisZeroIndex;
-                }
-                else
-                {
-                    //byteCount -= 18;
-                    printf("...glitch...");
-                }
-                zeroCount = 0; // reset counter so a start byte of value 0x00 doesn't trip this again...
-            }
-        }
-        else
-        {
-            if (zeroCount > 2 && zeroCount != 18)
-                printf("Found %d zeros in a row at location 0x%lX\n", zeroCount, (unsigned long)i);
-            zeroCount = 0;
+            // Add row to image (swapped frames, b+a, odd's first)
+            if (rowCount > 1016)
+                image[std::slice(width*static_cast<unsigned short>((rowCount-1017)*2),width,1)] = row; // + (unsigned short)rowCount;
+            else
+                image[std::slice(width*static_cast<unsigned short>(rowCount*2+1),width,1)] = row;
+            rowCount++;
         }
     }
 
+    printf("Done\r\n");
     // Save image
     long firstPixel(1);
     long exposure(1500);
